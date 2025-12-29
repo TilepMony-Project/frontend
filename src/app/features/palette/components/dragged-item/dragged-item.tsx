396d2320d870a5a0f15ae48b80fe235c499277f5
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2025-12-29 14:30:33 +0700

    fix: prevent drag preview from floating off-screen

--- src/app/features/palette/components/dragged-item/dragged-item.tsx
+++ src/app/features/palette/components/dragged-item/dragged-item.tsx
@@ -7,29 +7,30 @@ type DraggedItemProps = {
   zoom: number;
 };
 
-export const DraggedItem = forwardRef<HTMLDivElement, PropsWithChildren<DraggedItemProps>>(
-  ({ children, zoom }: PropsWithChildren<DraggedItemProps>, ref) => {
-    function getStyles() {
-      if (isSafari && !isOpera && !isChrome) {
-        return zoom > 1 ? { zoom: `${zoom}` } : { scale: `${zoom}` };
-      }
-
-      if (isFirefox) {
-        return { scale: `${zoom}` };
-      }
-
-      return { zoom: `${zoom}` };
+export const DraggedItem = forwardRef<
+  HTMLDivElement,
+  PropsWithChildren<DraggedItemProps>
+>(({ children, zoom }: PropsWithChildren<DraggedItemProps>, ref) => {
+  function getStyles() {
+    if (isSafari && !isOpera && !isChrome) {
+      return zoom > 1 ? { zoom: `${zoom}` } : { scale: `${zoom}` };
     }
 
-    return createPortal(
-      <div
-        ref={ref}
-        className="absolute bg-transparent origin-top-left z-[2] top-full"
-        style={getStyles()}
-      >
-        {children}
-      </div>,
-      document.body
-    );
+    if (isFirefox) {
+      return { scale: `${zoom}` };
+    }
+
+    return { zoom: `${zoom}` };
   }
-);
+
+  return createPortal(
+    <div
+      ref={ref}
+      className="absolute bg-transparent origin-top-left z-[2] -top-[10000px] -left-[10000px]"
+      style={getStyles()}
+    >
+      {children}
+    </div>,
+    document.body
+  );
+});
